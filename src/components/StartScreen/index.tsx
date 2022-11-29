7fa39cafef7f35576a06d9719524715d4089645e
Perri Schofield <perrischofield@gmail.com>
2022-11-29 21:43:24 +0000

    Bring loading forward, add Metal mesh to CoinSlot class

--- src/components/StartScreen/index.tsx
+++ src/components/StartScreen/index.tsx
@@ -1,8 +1,8 @@
-import { useEffect, useState, useRef, useCallback } from "react";
+import { useEffect, useRef, useCallback } from "react";
 import gsap from "gsap";
 
 import { Load } from "@/webgl/load";
-import { GL_START_VENDING_MACHINE, LOAD_COMPLETE } from "@/webgl/config/topics";
+import { GL_START_VENDING_MACHINE } from "@/webgl/config/topics";
 
 import CoinSlot from "@/components/CoinSlot";
 
@@ -10,10 +10,8 @@ import { StartScreenWrapper } from "./styles";
 
 const StartScreen = () => {
   const startScreenRef = useRef<HTMLDivElement>(null);
-  const [isLoaded, setIsLoaded] = useState<boolean>(true);
 
   const handleSubscriptions = useCallback(() => {
-    PubSub.subscribe(LOAD_COMPLETE, () => setIsLoaded(false));
     PubSub.subscribe(GL_START_VENDING_MACHINE, () => removeStartScreen());
   }, []);
 
@@ -25,7 +23,6 @@ const StartScreen = () => {
 
   const removeStartScreen = () => {
     gsap.to(startScreenRef.current, {
-      delay: 1,
       duration: 1,
       opacity: 0,
       onComplete: () => {
@@ -38,7 +35,6 @@ const StartScreen = () => {
 
   return (
     <StartScreenWrapper ref={startScreenRef}>
-      {isLoaded && <p>LOADING</p>} {!isLoaded && <p>LOADED</p>}
       <CoinSlot />
     </StartScreenWrapper>
   );
--- src/webgl/worldCoinSlot.ts
+++ src/webgl/worldCoinSlot.ts
@@ -1,5 +1,12 @@
-import { Mesh, MeshBasicMaterial, PlaneGeometry, Group } from "three";
-import { GLTF, GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import {
+  Texture,
+  Group,
+  Mesh,
+  MeshBasicMaterial,
+  PlaneGeometry,
+  MeshStandardMaterial,
+  EquirectangularReflectionMapping,
+} from "three";
 import gsap from "gsap";
 
 import { Renderer } from "./globals/Renderer";
@@ -7,16 +14,23 @@ import { Scene } from "./globals/Scene";
 import { Camera } from "./globals/Camera";
 import { AmbientLight } from "./globals/AmbientLight";
 import { DirectionalLight } from "./globals/DirectionalLight";
-import { GL_INSERT_COIN, GL_START_VENDING_MACHINE } from "./config/topics";
+import {
+  LOAD_COMPLETE,
+  GL_INSERT_COIN,
+  GL_START_VENDING_MACHINE,
+} from "./config/topics";
+import { AssetController } from "./controllers/AssetController";
 
 export class worldCoinSlot {
+  assetController = AssetController.getInstance();
+
   renderer: Renderer;
   camera: Camera;
   scene: Scene;
   ambientLight: AmbientLight;
   directionalLight: DirectionalLight;
 
-  gltfLoader: GLTFLoader;
+  hdr?: Texture;
   coin?: Group;
   canvasParent: HTMLDivElement;
 
@@ -27,56 +41,59 @@ export class worldCoinSlot {
     this.ambientLight = new AmbientLight();
     this.directionalLight = new DirectionalLight();
 
-    this.gltfLoader = new GLTFLoader();
-
     this.canvasParent = canvasParent;
 
-    this.load();
+    this.handleSubscriptions();
   }
 
-  loadModel(url: string) {
-    return new Promise<GLTF>((resolve) => {
-      this.gltfLoader.load(url, resolve);
-    });
-  }
+  init() {
+    if (!this.assetController.models) return;
 
-  async load() {
-    const gltf = await this.loadModel("/models/placeholder_coin.glb");
-
-    this.coin = gltf.scene;
-    this.coin.position.z = 1.5;
+    this.coin = this.assetController.models[3].clone();
+    this.coin.position.z = 3;
+    this.coin.position.y = -2;
     this.coin.rotation.x = Math.PI / 2;
     this.coin.rotation.z = Math.PI / 2;
     this.coin.scale.setScalar(0.25);
 
     this.scene.add(this.coin);
 
-    this.init();
-  }
+    if (this.assetController.hdrs) this.hdr = this.assetController.hdrs[0];
+    if (this.hdr) this.hdr.mapping = EquirectangularReflectionMapping;
 
-  init() {
     this.renderer.setAspectRatio(this.canvasParent);
     this.camera.setAspectRatio(this.canvasParent);
 
     this.scene.add(this.ambientLight);
     this.scene.add(this.directionalLight);
 
-    this.camera.position.set(0, 2, 3);
+    this.camera.position.set(0, 0, 5);
     this.camera.lookAt(0, 0, 0);
 
     this.renderer.setAnimationLoop(() => this.render());
     this.canvasParent.appendChild(this.renderer.domElement);
 
     this.addEntities();
-
-    this.handleSubscriptions();
   }
 
   handleSubscriptions() {
+    PubSub.subscribe(LOAD_COMPLETE, () => this.init());
     PubSub.subscribe(GL_INSERT_COIN, () => this.insertCoin());
   }
 
   addEntities() {
+    const metalGeometry = new PlaneGeometry(2, 3, 1);
+    const metalMaterial = new MeshStandardMaterial({
+      color: 0xc0c0c0,
+      roughness: 0.85,
+      metalness: 1,
+      envMap: this.hdr,
+    });
+
+    const metal = new Mesh(metalGeometry, metalMaterial);
+    metal.position.z = -0.01;
+    this.scene.add(metal);
+
     const planeGeometry = new PlaneGeometry(0.25, 1, 1);
     const planeMaterial = new MeshBasicMaterial({ color: 0x000000 });
     const plane = new Mesh(planeGeometry, planeMaterial);
@@ -86,13 +103,28 @@ export class worldCoinSlot {
   insertCoin() {
     if (!this.coin) return;
 
-    gsap.to(this.coin.position, {
+    const lmao = gsap.to(this.coin.position, {
       delay: 0,
-      duration: 3,
-      z: -1,
+      duration: 1,
+      z: 3,
+      y: 0,
+      ease: "back.out(3)",
       onComplete: () => {
-        document.body.style.overflow = "scroll";
-        PubSub.publish(GL_START_VENDING_MACHINE);
+        if (!this.coin) return;
+
+        gsap.to(this.coin.position, {
+          delay: 0,
+          duration: 1.5,
+          z: -1,
+          y: 0,
+          ease: "expo.inOut",
+          onComplete: () => {
+            lmao.kill();
+            document.body.style.overflow = "scroll";
+
+            PubSub.publish(GL_START_VENDING_MACHINE);
+          },
+        });
       },
     });
   }

f1f900688ead33bc931acf5cfeef106f05fa9e3f
Perri Schofield <perrischofield@gmail.com>
2023-03-13 21:24:42 +0000

    Remove credit content and animations from StartScreen

--- src/components/StartScreen/styles.tsx
+++ src/components/StartScreen/styles.tsx
@@ -1,9 +1,7 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { motion } from "framer-motion";
 
 import * as color from "@/styles/globals/color";
-import * as type from "@/styles/globals/type";
-import * as spacing from "@/styles/globals/spacing";
 import * as breakpoint from "@/styles/globals/breakpoints";
 
 export const StartScreenWrapper = styled(motion.div)`
@@ -21,60 +19,18 @@ export const StartScreenWrapper = styled(motion.div)`
   background-color: ${color.COSMIC_AMERICANO};
 `;
 
-export const ContentContainer = styled.div`
+export const CoinSlotContainer = styled(motion.div)`
   display: flex;
-
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-`;
-
-export const Title = styled.h1`
-  display: flex;
-  margin: 0;
-  padding: ${spacing.MARGIN_SMALL} ${spacing.MARGIN_LARGE};
-
-  overflow: hidden;
-  color: ${color.VERMILLION};
-  text-shadow: 2px 2px ${color.COSMIC_LATTE};
-
-  ${type.TYPE_CURSIVE_HEADER_1}
-`;
-
-export const TitleSpan = styled(motion.span)`
-  padding: 0 30px;
-  margin: 0 -30px;
-`;
-
-export const Credit = styled(motion.h2)`
-  margin: -${spacing.MARGIN_SMALL} 0 0;
-  padding: 0;
-
-  color: ${color.COSMIC_LATTE};
-
-  ${type.TYPE_SANS_BODY_1}
-`;
-
-interface CoinSlotContainerProps {
-  $ready: boolean;
-}
-
-export const CoinSlotContainer = styled(motion.div)<CoinSlotContainerProps>`
-  display: flex;
-  width: 35vw;
-  height: 35vw;
+  width: 50vw;
+  height: 50vw;
   aspect-ratio: 1/1;
+
+  &:hover {
+    cursor: pointer;
+  }
 
   @media (max-width: ${breakpoint.MOBILE_MAX}) {
     width: 100vw;
     height: 100vw;
   }
-
-  ${(props) =>
-    props.$ready &&
-    css`
-      &:hover {
-        cursor: pointer;
-      }
-    `}
 `;
